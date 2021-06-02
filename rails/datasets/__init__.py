0b076fe159e713fb3d6117e40fa9833d14e49de9
Dian Chen <10444308+dotchen@users.noreply.github.com>
2021-06-02 14:56:26 -0500

    Update __init__.py

--- rails/datasets/__init__.py
+++ rails/datasets/__init__.py
@@ -14,28 +14,6 @@ def data_loader(data_type, config):
     else:
         raise NotImplementedError(f'Unknown data type {data_type}')
 
-    if config.balanced_cmd:
-        # cmds = defaultdict(list)
-        # for idx in tqdm.tqdm(range(0,len(dataset),3), 'Computing class weights'):
-        # # for idx in tqdm.tqdm(range(0,300,3), 'Computing class weights'):
-        #     cmd = dataset[idx][-1]
-        #     cmds[cmd].append(idx)
-
-        # weights = np.zeros(len(dataset))
-        # for cmd, idxes in cmds.items():
-        #     print (cmd, len(idxes))
-        #     np_idxes = np.array(idxes)
-        #     weights[np_idxes] = len(dataset)/len(idxes)
-        #     weights[np_idxes+1] = len(dataset)/len(idxes)
-        #     weights[np_idxes+2] = len(dataset)/len(idxes)
-        
-        # import pickle
-        # pickle.dump(weights, open('weights.pkl', 'wb'))
-        import pickle
-        weights = pickle.load(open('weights.pkl', 'rb'))
-        
-        return DataLoader(dataset, batch_size=config.batch_size, num_workers=config.num_workers, drop_last=True, sampler=WeightedRandomSampler(weights, len(weights)))
-    else:
-        return DataLoader(dataset, batch_size=config.batch_size, num_workers=config.num_workers, shuffle=True, drop_last=True)
+    return DataLoader(dataset, batch_size=config.batch_size, num_workers=config.num_workers, shuffle=True, drop_last=True)
 
 __all__ = ['data_loader']
